e93df6afb7045a9dd8506587297ccb98b662151b
LARSS <larss@larss-Precision-WorkStation-670.(none)>
2013-06-20 15:26:24 -0400

    Removed old MPSettings class

--- mavproxy.py
+++ mavproxy.py
@@ -20,67 +20,6 @@ sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), '..
 from MAVProxy.modules.lib import textconsole
 from MAVProxy.modules.lib import mp_settings
 
-class MPSettings(object):
-    def __init__(self):
-        self.vars = [ ('link', int),
-                      ('altreadout', int),
-                      ('distreadout', int),
-                      ('battreadout', int),
-                      ('heartbeat', int),
-                      ('numcells', int),
-                      ('speech', int),
-                      ('mavfwd', int),
-                      ('mavfwd_rate', int),
-                      ('streamrate', int),
-                      ('streamrate2', int),
-                      ('heartbeatreport', int),
-                      ('radiosetup', int),
-                      ('paramretry', int),
-                      ('moddebug', int),
-                      ('rc1mul', int),
-                      ('rc2mul', int),
-                      ('rc4mul', int)]
-        self.link = 1
-        self.altreadout = 10
-        self.distreadout = 200
-        self.battreadout = 0
-        self.basealtitude = -1
-        self.heartbeat = 1
-        self.numcells = 0
-        self.mavfwd = 1
-        self.mavfwd_rate = 0
-        self.speech = 0
-        self.streamrate = 4
-        self.streamrate2 = 4
-        self.radiosetup = 0
-        self.heartbeatreport = 1
-        self.paramretry = 10
-        self.rc1mul = 1
-        self.rc2mul = 1
-        self.rc4mul = 1
-        self.moddebug = 0
-
-    def set(self, vname, value):
-        '''set a setting'''
-        for (v,t) in sorted(self.vars):
-            if v == vname:
-                try:
-                    value = t(value)
-                except:
-                    print("Unable to convert %s to type %s" % (value, t))
-                    return
-                setattr(self, vname, value)
-                return
-
-    def show(self, v):
-        '''show settings'''
-        print("%20s %s" % (v, getattr(self, v)))
-
-    def show_all(self):
-        '''show all settings'''
-        for (v,t) in sorted(self.vars):
-            self.show(v)
-
 class MPStatus(object):
     '''hold status information about the mavproxy'''
     def __init__(self):
