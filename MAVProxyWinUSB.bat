720055cb2e3e9efcb99039f9f9566e1bc984378d
Ben Holden <bsholden@ncsu.edu>
2013-06-27 09:02:31 -0400

    Revert "Fixed BAT file"
    This reverts commit 0f95683778cc5121f449256c5606ff910981972b.

--- MAVProxyWinUSB.bat
+++ MAVProxyWinUSB.bat
@@ -1,6 +1,6 @@
 setlocal
 SET PATH=%PATH%;..\mavlink;..\mavlink\pymavlink\examples
 
-mavproxy.py --master=COM13 --baudrate=57600 --target-system=17
+mavproxy.py --master=COM13 --baudrate=57600
 
 pause
