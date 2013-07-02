db7b42b987ca7bca307216058b05a23f1baeb330
Dominick C. Pastore <dominick.pastore@live.longwood.edu>
2013-07-02 16:40:17 -0400

    Continued refining

--- masterselect.py
+++ masterselect.py
@@ -2,33 +2,51 @@
 
 import wx
 
-class MasterSelect(wx.Frame):
+class MasterSelectDialog(wx.Frame):
     def __init__(self, *args, **kwargs):
-        super(MasterSelect, self).__init__(*args, **kwargs)
+        super(MasterSelectDialog, self).__init__(*args, **kwargs)
         self.init_ui()
 
-        self.SetSize((600, 400))
+        self.SetSize((500, 400))
         self.SetTitle("Select MAVLink master")
         self.Centre()
         self.Show(True)
 
     def init_ui(self):
-        panel = wx.Panel(self)
         vbox = wx.BoxSizer(wx.VERTICAL)
 
-        hbox_label = wx.BoxSizer(wx.HORIZONTAL)
-        lbl1 = wx.StaticText(panel, label="Serial interface")
-        hbox_label.Add(lbl1, flag=wx.EXPAND|wx.RIGHT|wx.LEFT, border=8)
-        lbl2 = wx.StaticText(panel, label="Detected system/components")
-        hbox_label.Add(lbl2, flag=wx.EXPAND|wx.RIGHT|wx.LEFT, border=8)
-        vbox.Add(hbox_label, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=20)
+        fgrid = wx.FlexGridSizer(2, 2, 5, 5)
+        lbl1 = wx.StaticText(self, label="Serial interface")
+        lbl2 = wx.StaticText(self, label="Detected system/components")
+        self.if_list = wx.ListBox(self, -1, choices=["One", "Two", "Three"])
+        self.id_list = wx.ListBox(self, -1, choices=["Red", "Blue", "Green"])
+        fgrid.AddMany([(lbl1), (lbl2), (self.if_list, 1, wx.EXPAND), (self.id_list, 1, wx.EXPAND)])
+        fgrid.SetFlexibleDirection(wx.VERTICAL)
+        fgrid.SetNonFlexibleGrowMode(wx.FLEX_GROWMODE_ALL)
+        fgrid.AddGrowableCol(0, 0)
+        fgrid.AddGrowableCol(1, 0)
+        fgrid.AddGrowableRow(1, 1)
+        vbox.Add(fgrid, proportion=1, flag=wx.EXPAND|wx.ALL, border=8)
 
-        panel.SetSizer(vbox)
+        hbox_buttons = wx.BoxSizer(wx.HORIZONTAL)
+        btn_cancel = wx.Button(self, label="Cancel")
+        btn_ok = wx.Button(self, label="OK")
+        hbox_buttons.Add(btn_cancel)
+        hbox_buttons.Add(btn_ok, flag=wx.LEFT, border=5)
+        vbox.Add(hbox_buttons, proportion=0, flag=wx.ALIGN_RIGHT|wx.ALL, border=8)
 
-def master_select_init():
-    app = wx.App()
-    MasterSelect(None)
-    app.MainLoop()
+        self.SetSizer(vbox)
+
+class MasterSelect(object):
+    def __init__(self):
+
+    def master_select_init(self):
+        app = wx.App()
+        MasterSelectDialog(None)
+        app.MainLoop()
+
+    def #TODO
 
 if __name__ == '__main__':
-    master_select_init()
+    ms = MasterSelect()
+    ms.master_select_init()
